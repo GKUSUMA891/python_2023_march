f42dd6f20758a527e603deec183dcf1d5cd4ace8
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-02 12:37:44 +0530

    even or odd number

---
+++ even_odd.py
@@ -0,0 +1,15 @@
+#even or odd
+'''num=int(input("enter number:"))
+if num % 2==0:
+    print("even number")
+elif num %2!=0:
+    print("odd number")'''
+#--------------------------------------------------
+number=20
+if number %2!=0:
+    if number==0:
+        print("even")
+    else:
+        print("odd")
+else:
+    print("even number")
