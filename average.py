3b6be3fb9cbbc5ce31df523d7e3f25714aa902b9
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-02 12:22:32 +0530

    average and total

---
+++ average.py
@@ -0,0 +1,10 @@
+English=int(input("enter english marks:"))
+Kannada=int(input("enter kannada marks:"))
+Maths=int(input("enter emaths marks:"))
+Sanskrit=int(input("enter sanskrit marks:"))
+Hindi=int(input("enter hindi marks:"))
+Social=int(input("enter social marks:"))
+total=English+Kannada+Maths+Sanskrit+Hindi+Social
+average=total/6
+print("Total marks of the six subjects are:",total)
+print("average marks of the six subjects are:",average)
