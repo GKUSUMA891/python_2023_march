962b169440cad9f7ba8ae0274a0b923893d41870
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-02 12:39:56 +0530

    area of rectangle

---
+++ area_rectangle.py
@@ -0,0 +1,6 @@
+length=int(input("Enter value of length:"))
+bredth=int(input("Enter value of length:"))
+Area=length * bredth
+perimeter= 2*(length + bredth)
+print("Area of rectangle is: ", Area)
+print("peri
