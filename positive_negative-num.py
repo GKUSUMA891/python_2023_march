2a507936f65b64a7278f75ec7646ce44eaec59cd
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-02 12:45:47 +0530

    positive number and negative number

---
+++ positive_negative-num.py
@@ -0,0 +1,17 @@
+#positive and negative
+number=-20
+if number > 0:
+    print("positive number")
+elif number < 0:
+    print("Negative number")
+else:
+    print("zero")
+#---------------------------------------------------
+num=int(input("enter number:"))
+if num >=0:
+    if num>0:
+        print("Positive number")
+    elif num==0:
+        print("zero")
+else:
+    print("negative number")
