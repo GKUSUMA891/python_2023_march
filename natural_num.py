7253e4e5ea0e777e9fb2eb9a601dd08ed8b9deea
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-02 12:20:48 +0530

    sum of natural numbers

---
+++ natural_num.py
@@ -0,0 +1,25 @@
+#sum of natural numbers and sum of first n natural numbers.
+'''num=5
+sum=0
+for i in range(num+1):
+    sum+=i
+    print("sum of natural num is:",sum)
+#--------------------------------------
+n=50
+sum=0
+for i in range(1,num+1):
+    sum+=i
+print("sum of natural num is:",sum)
+--------------------------------------
+num=int(input("enter number"))\
+for i in range(num+1):
+    sum=(num*(num+1))/2
+print("sum of natural num is:",sum)
+------------------------------------------------------------------'''
+#sum of natural numbers in a given range
+number1=5
+number2=12
+sum=0
+for i in range(number1,number2):
+    sum+=i
+print("sum of natural numbers in a range",sum)
