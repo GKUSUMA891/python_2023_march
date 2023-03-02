cba3ddcd89efa3ea1f602716eeddf970897018da
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-02 12:00:47 +0530

    operators

---
+++ operators/operators.py
@@ -0,0 +1,61 @@
+#operators & precedence
+
+x=123
+y=12
+print(x+y) #135
+print(x+7)  #130
+print(x-12)  #111
+print(x*y)      #1476
+print(x/2)  #61.5    =>returns float(/)
+print(x//2)   #61   =>returns integer (//)
+print(x%2)    #1   =>returns reminder
+xx=10
+xy=12
+print(xx**2)    #100  =>power(10*10)
+xz=xx+20       #=>incrementing
+xx+=30         #xx=xx+30
+x-=5         #x=x-5
+print(xz)   #30
+print(x)   # 118       
+print(xx)   #40
+
+var=(10+20)*25-12    #=>(30)*25-12 =>750-12 =>738
+print(var)  #738
+
+var1=(10*2)/(12+5)
+var2=(10*2)/(12+5)*20
+print(var1)      #1.176  =>20/17 =>1.17
+print(var2)     #23.529 =>
+maxi=10+3*2**2
+print(maxi)   #22 =>10+3*4  =>10+12 =>22
+
+a=(10+3)*2**2
+print(a)     #52  =>13*4  =>52
+
+b=10+3*2**2
+print(b)    #22     =>10+3*4  =>10+12  =>22
+
+c=(2+3)*10-3
+print(c)  #47     =>(5)*10-3  =>50-3  =>47
+a=5
+b=4
+#greater than operation
+print(a>b)
+
+#less than operation
+print(a<b)
+
+#greater than equal to operation
+print(a>=b)
+
+#less than or equal to operation
+print(a<=b)
+
+#equal to operation
+print(a==b)
+
+#not equal to operation
+print(a!=b)
+
+
+
