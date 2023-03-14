9e3f46abf4f9768eaa53602a97bb599f43f3d796
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-14 20:21:29 +0530

    operator_precedence

--- operator_precd.py
+++ operator_precd.py
@@ -1,3 +1,24 @@
+
+8-5+8/3==65/10-2+5%2  
+8-5+2==5/10-2+5%2    first / priority  
+8-5+2==6-2+5%2     first2nd step: / priority 
+8-5+2==6-2+1    third step:  % priority 
+3+2==4+1        fourth step: - priority 
+5==5       fifth step:  + priority 
+result= 1
+-------------------------------------------------------------------------------
+(2//3+4*2%3-1**9**8)   
+here ** have the same precedency so wee need to go t54o associtivity so Right to left 
+1st   1**9**8=>1
+next follor division(//) Left to Right so 
+2nd   2//3=>0
+ 0+4*2%3-1*1  
+ =>0+8%3-1*1 =>next 8%3  =>2  
+next 0+2-1*1 
+=>1*1=>1  0+2-1 =>1
+next 0+1*1=>  1*1=>1  
+o/p=1
+---------------------------------------------------
 l = 15
 m = 109
 n = 152
