119a8dedaafa58711a9bca11cf7b54c4080c1e7b
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2023-03-15 22:58:53 +0530

    control structure programs

---
+++ control_struc.py
@@ -0,0 +1,19 @@
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+c=int(input("enter c value:"))
+if a>b and a>c:
+      print("a is largest")
+if b>a and b>c:
+      print("b is largest")
+if c>a and c>b:
+      print("c is largest")
+if a==b:
+    print('a and b are same')
+elif a!=b:
+    print("a and b are not same")
+result:
+        enter a value:12
+        enter b value:3
+        enter c value:44
+        c is largest
+        a and b are not same
